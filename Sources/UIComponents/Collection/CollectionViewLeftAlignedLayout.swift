bceea695b8f8a17b7017eb59902a67f115b9a5a5
Ilya Kuznetsov <i.v.kuznecov@gmail.com>
2022-11-19 20:19:15 +0100

    fix left aligned layout

--- Sources/UIComponents/Collection/CollectionViewLeftAlignedLayout.swift
+++ Sources/UIComponents/Collection/CollectionViewLeftAlignedLayout.swift
@@ -14,14 +14,14 @@ class CollectionViewLeftAlignedLayout: UICollectionViewFlowLayout {
         var leftMargin = sectionInset.left
         var maxY: CGFloat = -1.0
         attributes?.forEach { layoutAttribute in
-            if layoutAttribute.frame.origin.y >= maxY {
+            if floor(layoutAttribute.frame.origin.y) >= maxY {
                 leftMargin = sectionInset.left
             }
 
             layoutAttribute.frame.origin.x = leftMargin
 
             leftMargin += layoutAttribute.frame.width + minimumInteritemSpacing
-            maxY = max(layoutAttribute.frame.maxY , maxY)
+            maxY = floor(max(layoutAttribute.frame.maxY , maxY))
         }
         return attributes
     }
